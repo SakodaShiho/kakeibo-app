c8bba76225508f58e23c783c1284410cbfce7006
“SakodaShiho” <sawadasakura322@gmail.com>
2021-07-15 17:32:19 +0900

    login　slippage　fix

--- src/auth/AuthProvider.js
+++ src/auth/AuthProvider.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { auth } from '../firebase';
 
-// contextの作成
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // ユーザーをログインさせる関数
   const login = async (email, password, history) => {
     try {
       await auth.signInWithEmailAndPassword(email, password);
@@ -18,7 +16,6 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  // 新しいユーザーを作成しログインさせる関数
   const signup = async (email, password, history) => {
     try {
       await auth.createUserWithEmailAndPassword(email, password);
@@ -48,7 +45,6 @@ export const AuthProvider = ({ children }) => {
   auth.onAuthStateChanged(setCurrentUser);
 
   return (
-    // Contextを使用して認証に必要な情報をコンポーネントツリーに流し込む。
     <AuthContext.Provider value={value}>
       {!loading && children}
     </AuthContext.Provider>
